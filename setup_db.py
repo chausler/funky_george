64afdbb7e043b9218ebaf514e5d2f64dd4bdc711
chausler <chausler@gmail.com>
2013-07-10 16:18:36 +0200

    added setup_db

---
+++ setup_db.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+"""
+load some config values into the database
+@author: Chris Hausler
+"""
+from config import db_name
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client[db_name]
+db.config.insert({'name': 'access_token_key',
+                                   'value': ''})
+db.config.insert({'name': 'access_token_secret',
+                                   'value': ''})
+db.config.insert({'name': 'consumer_key',
+                                   'value': ''})
+db.config.insert({'name': 'consumer_secret',
+                                   'value': ''})
